a831af3163389f3cfc652a2e96f011203f09e855
mouzarthy <mouzarthy2007@hotmail.com>
2025-07-22 21:35:07 -0300

    L30

--- memory.v
+++ memory.v
@@ -14,5 +14,25 @@ module Memory #(
     output wire        ack_o       // Confirmação da transação
 );
 
+reg [31:0]  mem[0:MEMORY_SIZE];
+
+initial 
+begin
+    if( MEMORY_FILE != "" )
+    begin
+        $readmemh( MEMORY_FILE, mem );
+    end    
+end
+
+always @( posedge clk ) 
+begin
+    if( wr_en_i == 1 )
+    begin
+        mem[addr_i[31:2]] <= data_i;
+    end    
+end
+
+assign data_o   = ( rd_en_i ) ? mem[addr_i[31:2]] : 32'bx;
+assign ack_o    = wr_en_i | rd_en_i;
 
 endmodule
